d51af6889e5f41347c65f86e295722940ad4c499
Roshan <roshanlamtari832@gmail.com>
2026-06-03 10:04:09 +0545

    Updated

---
+++ reactapp/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-slate-900 text-white">
+      <div className="p-8 bg-slate-800 rounded-2xl shadow-xl border border-slate-700 text-center">
+        <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r Kahn from-teal-400 to-emerald-400 mb-2">
+          Setup Complete!
+        </h1>
+        <p className="text-slate-400 text-lg">
+          React + Tailwind CSS v4 are running smoothly on Windows.
+        </p>
+        <p className="text-lg">Hello i am roshan</p>
+      </div>
+    </div>
+  )
+}
+
+export default app
